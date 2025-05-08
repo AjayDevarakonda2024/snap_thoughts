cd2897f2fa0b21b017e7aa984d7920cf6d3b786d
Ajay Devarakonda <ajaydevarakonda150@gmail.com>
2025-05-08 23:15:18 +0530

    wwwww

--- app/page.tsx
+++ app/page.tsx
@@ -39,13 +39,14 @@ import Image from "next/image";
 
 // --- Firebase config ---
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD3f6Jj6u1xWDRop7MVk-NWhLbwqnPeHfA",
+  authDomain: "snap-thoughts-d1423.firebaseapp.com",
+  databaseURL: "https://snap-thoughts-d1423-default-rtdb.firebaseio.com",
+  projectId: "snap-thoughts-d1423",
+  storageBucket: "snap-thoughts-d1423.firebasestorage.app",
+  messagingSenderId: "736664268783",
+  appId: "1:736664268783:web:1be88e4d1d26ebd052d01a",
+  measurementId: "G-XYHPJ4PEPC"
 };
 
 // --- Types ---
